459deef4475e964f54885af1c9448ebf74ad6467
leneti <d.gudauskas1@gmail.com>
2023-01-08 15:17:12 +0000

    modified the app window to have header, footer and MantineProvider

---
+++ constants/theme.ts
@@ -0,0 +1,87 @@
+import {
+  MantineTheme,
+  MantineThemeOverride,
+  ColorScheme,
+  ButtonStylesParams,
+} from "@mantine/core";
+
+export const getTheme = (colorScheme: ColorScheme): MantineThemeOverride => ({
+  colorScheme,
+  colors: {
+    brand:
+      colorScheme === "dark"
+        ? [
+            "#FFF3EA",
+            "#FFDFC6",
+            "#FFCDA4",
+            "#FFBC85",
+            "#FFAB66",
+            "#FF9B49",
+            "#FF8D2F",
+            "#FF8017",
+            "#FF7301",
+            "#EB6A00",
+          ]
+        : [
+            "#B2E1DC",
+            "#40BCAF",
+            "#247A71",
+            "#134F49",
+            "#0A332F",
+            "#05221F",
+            "#021614",
+            "#010808",
+            "#010303",
+            "#000101",
+          ],
+    "brand-orange": [
+      "#FFF3EA",
+      "#FFDFC6",
+      "#FFCDA4",
+      "#FFBC85",
+      "#FFAB66",
+      "#FF9B49",
+      "#FF8D2F",
+      "#FF8017",
+      "#FF7301",
+      "#EB6A00",
+    ],
+    "brand-green": [
+      "#B2E1DC",
+      "#40BCAF",
+      "#247A71",
+      "#134F49",
+      "#0A332F",
+      "#05221F",
+      "#021614",
+      "#010808",
+      "#010303",
+      "#000101",
+    ],
+    "brand-light-green": [
+      "#FEFEFE",
+      "#E9EBE3",
+      "#D5D9CB",
+      "#C3C9B5",
+      "#B1B99F",
+      "#A1AA8B",
+      "#929D79",
+      "#85916A",
+      "#78825F",
+      "#6C7556",
+    ],
+  },
+  primaryShade: { light: 5, dark: 4 },
+  primaryColor: "brand",
+  components: {
+    Button: {
+      defaultProps: {
+        styles: (theme: MantineTheme, params: ButtonStylesParams) => ({
+          root: {
+            color: params.variant === "gradient" ? "#fff" : theme.colorScheme === "dark" ? "#000" : "#fff",
+          },
+        }),
+      },
+    },
+  },
+});
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,91 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useState } from "react";
+import { GetServerSidePropsContext } from "next";
+import { AppProps } from "next/app";
+import { getCookie, setCookie } from "cookies-next";
+import {
+  MantineProvider,
+  ColorScheme,
+  ColorSchemeProvider,
+} from "@mantine/core";
+import HeaderMenu from "@components/Header";
+import FooterMenu from "@site/components/Footer";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { Link } from "@site/models/header";
+import { getTheme } from "@site/constants/theme";
+
+const links: Link[] = [
+  {
+    label: "Apie mus",
+    link: "/apie-mus",
+  },
+  {
+    label: "Apsaugos sprendimai",
+    link: "#",
+  },
+  {
+    label: "Kontaktai",
+    link: "/kontaktai",
+  },
+  {
+    label: "Paslaugos",
+    links: [
+      {
+        label: "Apsaugos signalizacijos sistemos",
+        link: "#",
+      },
+      {
+        label: "Įeigos kontrolės sistemos",
+        link: "#",
+      },
+      {
+        label: "Integruoti apsaugos sprendimai",
+        link: "#",
+      },
+      {
+        label: "Priešgaisrinės signalizacijos sistemos",
+        link: "#",
+      },
+      {
+        label: "Vaizdo stebėjimo sistemos",
+        link: "#",
+      },
+    ],
+  },
+];
+
+export default function App(props: AppProps & { colorScheme: ColorScheme }) {
+  const { Component, pageProps } = props;
+  const [colorScheme, setColorScheme] = useState<ColorScheme>(
+    props.colorScheme
+  );
+
+  const toggleColorScheme = (value?: ColorScheme) => {
+    const nextColorScheme =
+      value || (colorScheme === "dark" ? "light" : "dark");
+    setColorScheme(nextColorScheme);
+    setCookie("mantine-color-scheme", nextColorScheme, {
+      maxAge: 60 * 60 * 24 * 30,
+    });
+  };
+
+  return (
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
+    >
+      <MantineProvider
+        theme={getTheme(colorScheme)}
+        withGlobalStyles
+        withNormalizeCSS
+      >
+        <HeaderMenu links={links} />
+        <Component {...pageProps} />
+        <FooterMenu />
+      </MantineProvider>
+    </ColorSchemeProvider>
+  );
 }
+
+App.getInitialProps = ({ ctx }: { ctx: GetServerSidePropsContext }) => ({
+  colorScheme: getCookie("mantine-color-scheme", ctx) || "light",
+});
