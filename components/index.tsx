7893a614897d8e91ba90e4b1ed0029a50101d6da
leneti <d.gudauskas1@gmail.com>
2023-03-12 11:46:54 +0000

    added text to about us page

--- components/index.tsx
+++ components/index.tsx
@@ -1,6 +1,6 @@
 export { default as ContactInfo } from "./ContactInfo";
 export { default as Footer } from "./Footer";
-export { default as GetInTouch } from "./GetInTouch";
+export { default as GetInTouch, type ContactForm } from "./GetInTouch";
 export { default as Header } from "./Header";
 export { default as Hero } from "./Hero";
 export { default as Logo } from "./Logo";
