e51b37237db1c20c165f47cc20f186e5e3dde32e
leneti <d.gudauskas1@gmail.com>
2023-03-17 20:13:25 +0000

    exported slide down title to a reusable component

---
+++ components/SlideDownTitle/SlideDownTitle.tsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import { Title, Transition, type MantineTransition } from "@mantine/core";
+
+const scaleY: MantineTransition = {
+  in: {
+    opacity: 1,
+    transform: "scaleY(1)",
+    lineHeight: 1.3,
+    marginBottom: "1.5rem",
+  },
+  out: { opacity: 0, transform: "scaleY(0)", lineHeight: 0, marginBottom: 0 },
+  common: { transformOrigin: "top", fontWeight: 900 },
+  transitionProperty: "transform, opacity, line-height, margin-bottom",
+};
+
+export interface SlideDownTitleProps {
+  title: string;
+}
+
+export default function SlideDownTitle(props: SlideDownTitleProps) {
+  const { title } = props;
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
+
+  return (
+    <Transition mounted={mounted} transition={scaleY} duration={200}>
+      {(styles) => (
+        <Title align="center" style={styles}>
+          {title}
+        </Title>
+      )}
+    </Transition>
+  );
+}
