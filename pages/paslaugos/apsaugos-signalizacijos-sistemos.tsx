f4473e0f1bb54183f09ed41cc84e4600eedae406
leneti <d.gudauskas1@gmail.com>
2023-03-23 22:12:56 +0200

    copied over services text; converted md to html; made use of html in tsx

--- pages/paslaugos/apsaugos-signalizacijos-sistemos.tsx
+++ pages/paslaugos/apsaugos-signalizacijos-sistemos.tsx
@@ -1,7 +1,57 @@
-import { PageBackground } from "@components";
+import { Container, createStyles, Text } from "@mantine/core";
+import { PageTitle, SlideDownTitle } from "@components";
+import html from "./_page_content/ass.json";
+
+const useStyles = createStyles((theme) => ({
+  container: {
+    minHeight: 700,
+    backgroundColor: theme.fn.primaryColor(),
+    padding: `calc(${theme.spacing.xl} * 2) calc(${theme.spacing.xl} * 4)`,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+
+    [theme.fn.smallerThan("md")]: {
+      padding: `calc(${theme.spacing.xl} * 2) calc(${theme.spacing.xl} * 3)`,
+    },
+
+    [theme.fn.smallerThan("sm")]: {
+      padding: `calc(${theme.spacing.xl} * 2) ${theme.spacing.xl}`,
+    },
+  },
+
+  content: {
+    width: "75%",
+    maxWidth: theme.breakpoints.xl,
+
+    [theme.fn.smallerThan("md")]: {
+      width: "85%",
+    },
+
+    [theme.fn.smallerThan("sm")]: {
+      width: "100%",
+    },
+  },
+}));
 
 const pageTitle = "Apsaugos signalizacijos sistemos";
 
 export default function AlarmSystems() {
-  return <PageBackground pageTitle={pageTitle}></PageBackground>;
+  const { classes } = useStyles();
+
+  return (
+    <>
+      <PageTitle>{pageTitle}</PageTitle>
+
+      <SlideDownTitle title={pageTitle} />
+
+      <Container fluid className={classes.container}>
+        <Text
+          variant="lightBg"
+          className={classes.content}
+          dangerouslySetInnerHTML={html}
+        />
+      </Container>
+    </>
+  );
 }
