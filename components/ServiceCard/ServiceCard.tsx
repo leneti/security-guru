9534927eedf3182fe346030db16655957016176a
leneti <d.gudauskas1@gmail.com>
2023-04-24 00:35:47 +0100

    temporary display of services

---
+++ components/ServiceCard/ServiceCard.tsx
@@ -0,0 +1,50 @@
+import { Card, Image, Text, Badge, Button, Group, Title } from "@mantine/core";
+
+const CARD_HEIGHT = 400;
+const IMAGE_HEIGHT = 180;
+
+export default function ServiceCard({
+  image,
+  title,
+  description,
+  price,
+}: {
+  image: string;
+  title: string;
+  description: string;
+  price: string;
+}) {
+  return (
+    <Card
+      shadow="xl"
+      radius="md"
+      withBorder
+      h="100%"
+      mih={CARD_HEIGHT}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+      }}
+    >
+      <Card.Section>
+        <Image src={image} height={IMAGE_HEIGHT} alt={title} />
+      </Card.Section>
+
+      <Group position="apart" mt="md" mb="xs">
+        <Title order={3}>{title}</Title>
+        <Badge color="brand-light-green.6" variant="dot">
+          {price}
+        </Badge>
+      </Group>
+
+      <Text size="sm" color="dimmed">
+        {description}
+      </Text>
+
+      <Button variant="outline" fullWidth mt="md" radius="md">
+        Sužinoti daugiau
+      </Button>
+    </Card>
+  );
+}
