de58880c6f3ae480e9f8997875a072a112c0f455
leneti <d.gudauskas1@gmail.com>
2025-01-14 23:31:31 +0000

    remove lottie animation code

--- __tests__/app/apie-mus.test.tsx
+++ __tests__/app/apie-mus.test.tsx
@@ -1,9 +1,6 @@
 import About from "@site/app/apie-mus/page";
-import { render, screen } from "@site/test-utils";
+import { render } from "@site/test-utils";
 
-// jest.mock("@lottiefiles/react-lottie-player", () => ({
-//   Player: (props: any) => <div>Mock Player {JSON.stringify(props)}</div>,
-// }));
 jest.mock("@site/components/PageBackground");
 
 describe("About", () => {
@@ -13,7 +10,5 @@ describe("About", () => {
       .mockReturnValue(false);
 
     render(<About />);
-
-    // expect(screen.getByText(/mock player.*/i)).toBeInTheDocument();
   });
 });
