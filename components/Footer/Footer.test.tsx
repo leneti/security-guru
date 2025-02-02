d60e19e9dd693c96af67ca14c723e5fd8bf01de9
leneti <d.gudauskas1@gmail.com>
2025-02-02 21:01:32 +0000

    update tests

--- components/Footer/Footer.test.tsx
+++
@@ -1,9 +0,0 @@
-import { Footer } from "@site/components/Footer";
-import { render } from "@site/test-utils";
-
-describe("Footer", () => {
-  it("renders", () => {
-    const { container } = render(<Footer />);
-    expect(container).toMatchSnapshot();
-  });
-});
