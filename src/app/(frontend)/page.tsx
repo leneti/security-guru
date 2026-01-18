e8033152b60c2159232fde495b8cbd6053c8a750
Dominykas Gudauskas <d.gudauskas1@gmail.com>
2026-01-18 22:55:23 +0000

    Remove temporary PayloadCMS template web app

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,60 +1,15 @@
-import "./styles.css";
+import { AboutSection } from "@/components/AboutSection";
+import { ContactSection } from "@/components/ContactSection";
+import { HeroSection } from "@/components/HeroSection";
+import { ServicesSection } from "@/components/ServicesSection";
 
-import { fileURLToPath } from "url";
-
-import { headers as getHeaders } from "next/headers.js";
-import Image from "next/image";
-import { getPayload } from "payload";
-import React from "react";
-
-import config from "@payload-config";
-
-export default async function HomePage() {
-  const headers = await getHeaders();
-  const payload = await getPayload({ config });
-  const { user } = await payload.auth({ headers });
-
-  const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`;
-
+export default function HomePage() {
   return (
-    <div className="home">
-      <div className="content">
-        <picture>
-          <source srcSet="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg" />
-          <Image
-            alt="Payload Logo"
-            height={65}
-            src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg"
-            width={65}
-          />
-        </picture>
-        {!user && <h1>Welcome to your new project.</h1>}
-        {user && <h1>Welcome back, {user.email}</h1>}
-        <div className="links">
-          <a
-            className="admin"
-            href="/admin"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Go to admin panel
-          </a>
-          <a
-            className="docs"
-            href="https://payloadcms.com/docs"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Documentation
-          </a>
-        </div>
-      </div>
-      <div className="footer">
-        <p>Update this page by editing</p>
-        <a className="codeLink" href={fileURL}>
-          <code>app/(frontend)/page.tsx</code>
-        </a>
-      </div>
+    <div className="flex min-h-screen flex-col">
+      <HeroSection />
+      <ServicesSection />
+      <AboutSection />
+      <ContactSection />
     </div>
   );
 }
